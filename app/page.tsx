92870a31c60888bc244272767d123963bfadbe8f
Anthony Shew <anthony.shew@vercel.com>
2022-11-26 11:11:02 -0700

    Add revalidation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getHomePage } from "../lib/notion/getHomePage";
 import { BlocksRenderer } from "@/components/BlocksRenderer";
 
+export const revalidate = 1;
+
 export default async function Page() {
   const blockMap = await getHomePage();
 
