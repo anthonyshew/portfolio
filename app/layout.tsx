f517c29f00b15e7c35ea46f6f4bd0d10bf2eea0d
Anthony Shew <anthony.shew@vercel.com>
2022-11-27 16:17:33 -0700

    Add analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import { getNavigation } from "@/lib/notion";
 import { Navbar } from "@/components/Navbar";
+import { AnalyticsWrapper } from "@/components/AnalyticsWrapper";
 import { Inter } from "@next/font/google";
 
 const inter = Inter({
@@ -25,6 +26,7 @@ export default async function RootLayout({
       <body>
         <Navbar headerLinks={header} />
         <main className="pt-24 min-h-screen dark:bg-black">{children}</main>
+        <AnalyticsWrapper />
       </body>
     </html>
   );
